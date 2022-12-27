058f5f375ad9ac10b90e48bee42bbcbcf50166cf
Divine Asiriuwa <divine_asiriuwa@yahoo.com>
2022-12-27 21:30:44 +0100

    Add mobile menu function with javascript

---
+++ index.js
@@ -0,0 +1,21 @@
+document.getElementById('open-bar').onclick = function() {
+    document.getElementById('menu-bar').classList.add('show')
+    document.getElementById('open-bar').classList.add('close');
+    document.getElementById('close-bar').classList.remove('close');
+    document.getElementById('navi').classList.add('navi');
+    
+}
+
+document.getElementById('close-bar').onclick = function(){
+    document.getElementById('menu-bar').classList.remove('show')
+    document.getElementById('open-bar').classList.remove('close');
+    document.getElementById('close-bar').classList.add('close');
+    document.getElementById('navi').classList.remove('navi');
+}
+
+document.getElementById('toggle').onclick=function(){
+  document.getElementById('menu-bar').classList.remove('show');
+  document.getElementById('navi').classList.remove('navi');
+  document.getElementById('open-bar').classList.remove('close');
+  document.getElementById('close-bar').classList.add('close');
+};
