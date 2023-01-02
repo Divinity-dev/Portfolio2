072d795d8b16e73f33b1fd1cf22df86c8180e763
Divine Asiriuwa <divine_asiriuwa@yahoo.com>
2023-01-02 12:35:03 +0100

    added javascript

--- index.js
+++ index.js
@@ -20,6 +20,20 @@ document.getElementById('toggle').onclick=function(){
   document.getElementById('close-bar').classList.add('close');
 };
 
+document.getElementById('toggle1').onclick=function(){
+  document.getElementById('menu-bar').classList.remove('show');
+  // document.getElementById('navi').classList.remove('navi');
+  document.getElementById('open-bar').classList.remove('close');
+  document.getElementById('close-bar').classList.add('close');
+};
+
+document.getElementById('toggle2').onclick=function(){
+  document.getElementById('menu-bar').classList.remove('show');
+  // document.getElementById('navi').classList.remove('navi');
+  document.getElementById('open-bar').classList.remove('close');
+  document.getElementById('close-bar').classList.add('close');
+};
+
 const projectprop = [
   {name: 'My portfolio',
   Description: 'This is a simple portfolio website detailing  my skills, previous jobs and coding languages',
