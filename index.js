1904cc29cb22ad633a4b695a402bb73552ba96d3
Divine Asiriuwa <divine_asiriuwa@yahoo.com>
2023-01-02 21:57:22 +0100

    used arrow functions

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-// document.getElementById('open-bar').onclick = function open() {
-//   document.getElementById('menu-bar').classList.add('show');
-//   document.getElementById('open-bar').classList.add('close');
-//   document.getElementById('close-bar').classList.remove('close');
-// };
+document.getElementById('open-bar').onclick = () => {
+  document.getElementById('menu-bar').classList.add('show');
+  document.getElementById('open-bar').classList.add('close');
+  document.getElementById('close-bar').classList.remove('close');
+};
 
-// document.getElementById('close-bar').onclick = function close() {
-//   document.getElementById('menu-bar').classList.remove('show');
-//   document.getElementById('open-bar').classList.remove('close');
-//   document.getElementById('close-bar').classList.add('close');
-// };
+document.getElementById('close-bar').onclick = () => {
+  document.getElementById('menu-bar').classList.remove('show');
+  document.getElementById('open-bar').classList.remove('close');
+  document.getElementById('close-bar').classList.add('close');
+};
 
-// document.getElementById('toggle').onclick = function toggle() {
-//   document.getElementById('menu-bar').classList.remove('show');
-//   document.getElementById('open-bar').classList.remove('close');
-//   document.getElementById('close-bar').classList.add('close');
-// };
+document.getElementById('toggle').onclick = () => {
+  document.getElementById('menu-bar').classList.remove('show');
+  document.getElementById('open-bar').classList.remove('close');
+  document.getElementById('close-bar').classList.add('close');
+};
 
-// document.getElementById('toggle1').onclick = function toggle1() {
-//   document.getElementById('menu-bar').classList.remove('show');
-//   document.getElementById('open-bar').classList.remove('close');
-//   document.getElementById('close-bar').classList.add('close');
-// };
+document.getElementById('toggle1').onclick = () => {
+  document.getElementById('menu-bar').classList.remove('show');
+  document.getElementById('open-bar').classList.remove('close');
+  document.getElementById('close-bar').classList.add('close');
+};
 
-// document.getElementById('toggle2').onclick = function toggle2() {
-//   document.getElementById('menu-bar').classList.remove('show');
-//   document.getElementById('open-bar').classList.remove('close');
-//   document.getElementById('close-bar').classList.add('close');
-// };
+document.getElementById('toggle2').onclick = () => {
+  document.getElementById('menu-bar').classList.remove('show');
+  document.getElementById('open-bar').classList.remove('close');
+  document.getElementById('close-bar').classList.add('close');
+};
