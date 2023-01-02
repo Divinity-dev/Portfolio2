230932f1cb3301a31b57ebd105309ada18ed96aa
Divine Asiriuwa <divine_asiriuwa@yahoo.com>
2023-01-02 12:27:03 +0100

    Correct eslint errors

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
-document.getElementById('open-bar').onclick = function() {
-    document.getElementById('menu-bar').classList.add('show')
-    document.getElementById('open-bar').classList.add('close');
-    document.getElementById('close-bar').classList.remove('close');
-    // document.getElementById('navi').classList.add('navi');
-    
-}
+document.getElementById('open-bar').onclick = function () {
+  document.getElementById('menu-bar').classList.add('show');
+  document.getElementById('open-bar').classList.add('close');
+  document.getElementById('close-bar').classList.remove('close');
+};
 
-document.getElementById('close-bar').onclick = function(){
-    document.getElementById('menu-bar').classList.remove('show')
-    document.getElementById('open-bar').classList.remove('close');
-    document.getElementById('close-bar').classList.add('close');
-    // document.getElementById('navi').classList.remove('navi');
-}
+document.getElementById('close-bar').onclick = function () {
+  document.getElementById('menu-bar').classList.remove('show');
+  document.getElementById('open-bar').classList.remove('close');
+  document.getElementById('close-bar').classList.add('close');
+};
 
-document.getElementById('toggle').onclick=function(){
+document.getElementById('toggle').onclick = function () {
   document.getElementById('menu-bar').classList.remove('show');
   // document.getElementById('navi').classList.remove('navi');
+  document.getElementById('open-bar').classList.remove('close');
+  document.getElementById('close-bar').classList.add('close');
+};
+
+document.getElementById('toggle1').onclick = function () {
+  document.getElementById('menu-bar').classList.remove('show');
+  // document.getElementById('navi').classList.remove('navi');
+  document.getElementById('open-bar').classList.remove('close');
+  document.getElementById('close-bar').classList.add('close');
+};
+
+document.getElementById('toggle2').onclick = function () {
+  document.getElementById('menu-bar').classList.remove('show');
   document.getElementById('open-bar').classList.remove('close');
   document.getElementById('close-bar').classList.add('close');
 };
