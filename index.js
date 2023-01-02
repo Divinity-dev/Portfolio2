5d036f79d0590d386d134bb02134a162f2cf01a6
Divine Asiriuwa <divine_asiriuwa@yahoo.com>
2023-01-02 20:10:10 +0100

    corrected eslint error

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
-/* eslint-disable func-names */
-document.getElementById('open-bar').onclick = function () {
+document.getElementById('open-bar').onclick = function open() {
   document.getElementById('menu-bar').classList.add('show');
   document.getElementById('open-bar').classList.add('close');
   document.getElementById('close-bar').classList.remove('close');
 };
 
-// eslint-disable-next-line func-names
-document.getElementById('close-bar').onclick = function () {
+document.getElementById('close-bar').onclick = function close() {
   document.getElementById('menu-bar').classList.remove('show');
   document.getElementById('open-bar').classList.remove('close');
   document.getElementById('close-bar').classList.add('close');
 };
 
-// eslint-disable-next-line func-names
-document.getElementById('toggle').onclick = function () {
+document.getElementById('toggle').onclick = function toggle() {
   document.getElementById('menu-bar').classList.remove('show');
   document.getElementById('open-bar').classList.remove('close');
   document.getElementById('close-bar').classList.add('close');
 };
 
-document.getElementById('toggle1').onclick = function () {
+document.getElementById('toggle1').onclick = function toggle1() {
   document.getElementById('menu-bar').classList.remove('show');
   document.getElementById('open-bar').classList.remove('close');
   document.getElementById('close-bar').classList.add('close');
 };
 
-document.getElementById('toggle2').onclick = function () {
+document.getElementById('toggle2').onclick = function toggle2() {
   document.getElementById('menu-bar').classList.remove('show');
   document.getElementById('open-bar').classList.remove('close');
   document.getElementById('close-bar').classList.add('close');
