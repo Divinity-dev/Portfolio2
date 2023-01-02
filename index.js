593e37af1e2d6a7b4f56fc11bb81fb146247ee36
Divine Asiriuwa <divine_asiriuwa@yahoo.com>
2023-01-02 13:14:19 +0100

    Es lint correct

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// eslint-disable-next-line func-names
+/* eslint-disable func-names */
 document.getElementById('open-bar').onclick = function () {
   document.getElementById('menu-bar').classList.add('show');
   document.getElementById('open-bar').classList.add('close');
@@ -12,21 +12,18 @@ document.getElementById('close-bar').onclick = function () {
   document.getElementById('close-bar').classList.add('close');
 };
 
-// eslint-disable-next-line func-names
 document.getElementById('toggle').onclick = function () {
   document.getElementById('menu-bar').classList.remove('show');
   document.getElementById('open-bar').classList.remove('close');
   document.getElementById('close-bar').classList.add('close');
 };
 
-// eslint-disable-next-line func-names
 document.getElementById('toggle1').onclick = function () {
   document.getElementById('menu-bar').classList.remove('show');
   document.getElementById('open-bar').classList.remove('close');
   document.getElementById('close-bar').classList.add('close');
 };
 
-// eslint-disable-next-line func-names
 document.getElementById('toggle2').onclick = function () {
   document.getElementById('menu-bar').classList.remove('show');
   document.getElementById('open-bar').classList.remove('close');
