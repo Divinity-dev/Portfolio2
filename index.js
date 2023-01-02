77ee0f1b12a5fbddf1b1b1ff12af0e044d7a9a9e
Divine Asiriuwa <divine_asiriuwa@yahoo.com>
2023-01-02 12:50:19 +0100

    fixed eslint errors

--- index.js
+++ index.js
@@ -1,27 +1,32 @@
+// eslint-disable-next-line func-names
 document.getElementById('open-bar').onclick = function () {
   document.getElementById('menu-bar').classList.add('show');
   document.getElementById('open-bar').classList.add('close');
   document.getElementById('close-bar').classList.remove('close');
 };
 
+// eslint-disable-next-line func-names
 document.getElementById('close-bar').onclick = function () {
   document.getElementById('menu-bar').classList.remove('show');
   document.getElementById('open-bar').classList.remove('close');
   document.getElementById('close-bar').classList.add('close');
 };
 
+// eslint-disable-next-line func-names
 document.getElementById('toggle').onclick = function () {
   document.getElementById('menu-bar').classList.remove('show');
   document.getElementById('open-bar').classList.remove('close');
   document.getElementById('close-bar').classList.add('close');
 };
 
+// eslint-disable-next-line func-names
 document.getElementById('toggle1').onclick = function () {
   document.getElementById('menu-bar').classList.remove('show');
   document.getElementById('open-bar').classList.remove('close');
   document.getElementById('close-bar').classList.add('close');
 };
 
+// eslint-disable-next-line func-names
 document.getElementById('toggle2').onclick = function () {
   document.getElementById('menu-bar').classList.remove('show');
   document.getElementById('open-bar').classList.remove('close');
